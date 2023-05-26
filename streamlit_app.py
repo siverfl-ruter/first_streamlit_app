a7578c0f79ec781860f5c6ffaa74196e654e9dad
siverfl-ruter <134586254+siverfl-ruter@users.noreply.github.com>
2023-05-26 10:40:50 +0200

    Add fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,12 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 # Add frutyvice api call
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Normalize the json response and load to DF
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
